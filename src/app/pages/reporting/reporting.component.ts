93ed16ae1705ceb5223145ddd0390d7e63bf2eb6
Peter Duff <pdu@ihtsdo.org>
2019-01-30 16:34:00 +0000

    RP-120 Made table safe for unknown parameters

--- src/app/pages/reporting/reporting.component.ts
+++ src/app/pages/reporting/reporting.component.ts
@@ -55,11 +55,12 @@ export class ReportingComponent implements OnInit {
     }
 
     parameterValue(report, parameter) {
-        // console.log('REPORT: ', report);
-        // console.log('PARAM: ', parameter);
-        // console.log('VALUE: ', report.parameters.parameterMap[parameter.key].value);
-        // debugger;
-        return report.parameters.parameterMap[parameter.key].value;
+        if(report.parameters.hasOwnProperty('parameterMap') && report.parameters.parameterMap.hasOwnProperty(parameter.key)) {
+            return report.parameters.parameterMap[parameter.key].value;
+        }
+        else  {
+            return '';
+        }
     }
 
     setText(event) {
