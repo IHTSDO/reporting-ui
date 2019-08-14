4654b43e09aa4997d4648fb9a9edae21499f8a48
Peter Duff <pdu@ihtsdo.org>
2019-08-14 15:53:44 +0100

    RP-107 Resolving issues surrounding loading

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,6 +4,8 @@ import { AuthenticationService } from './services/authentication.service';
 // @ts-ignore
 import { version } from './../../package.json';
 
+import 'jquery';
+
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
@@ -23,6 +25,9 @@ export class AppComponent implements OnInit {
         this.authoringService.getUIConfiguration().subscribe(
             data => {
                 this.authoringService.uiConfiguration = data;
+
+                // $('<script>').attr({src: 'https://dev-workflow.ihtsdotools.org/s/eae63851c7444cb91c1a2fe49b048a36-T/9qqnuc/713005/8b99849fa1d8eaa169fd4a5dd7253186/2.0.31/_/download/batch/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector/com.atlassian.jira.collector.plugin.jira-issue-collector-plugin:issuecollector.js?locale=en&collectorId=dd01c5f4'}).appendTo('body');
+                $('<script>').attr({src: this.authoringService.uiConfiguration.endpoints.collectorEndpoint}).appendTo('body');
             },
             error => {
                 console.error('ERROR: UI Config failed to load');
--- src/app/models/uiConfiguration.ts
+++ src/app/models/uiConfiguration.ts
@@ -2,5 +2,6 @@ export class UIConfiguration {
     endpoints: {
         imsEndpoint: string;
         terminologyServerEndpoint: string;
+        collectorEndpoint: string;
     };
 }
