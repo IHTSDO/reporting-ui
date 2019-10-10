6ceb8406dd9e1ee1490b53afcfed553e01be2182
Peter Duff <pdu@ihtsdo.org>
2019-10-10 15:06:12 +0100

    RP-98 No results message for typeahead

--- src/app/services/terminologyServer.service.ts
+++ src/app/services/terminologyServer.service.ts
@@ -12,27 +12,6 @@ export class TerminologyServerService {
     constructor(private http: HttpClient, private authoringService: AuthoringService) {
     }
 
-    getTypeaheadConcepts(term): Observable<object> {
-        if (!term.trim()) {
-            return of({});
-        }
-
-        const params = {
-            termFilter: term,
-            limit: 20,
-            expand: 'fsn()',
-            activeFilter: true,
-            termActive: true
-        };
-
-        if (this.authoringService.uiConfiguration.endpoints.terminologyServerEndpoint.includes('snowowl')) {
-            delete params.termActive;
-        }
-
-        return this.http.post<object>(this.authoringService.uiConfiguration.endpoints.terminologyServerEndpoint +
-            'MAIN/concepts/search', params);
-    }
-
     getTypeahead(term) {
         const params = {
             termFilter: term,
@@ -47,9 +26,13 @@ export class TerminologyServerService {
             .pipe(map(responseData => {
                 const typeaheads = [];
 
-                responseData['items'].forEach((item) => {
-                    typeaheads.push(item.id + ' |' + item.fsn.term + '|');
-                });
+                if (responseData['items'].length > 0) {
+                    responseData['items'].forEach((item) => {
+                        typeaheads.push(item.id + ' |' + item.fsn.term + '|');
+                    });
+                } else {
+                    return ['No results found'];
+                }
 
                 return typeaheads;
             }));
