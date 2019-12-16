fa05ab22dd7da648eb9aab7b79016aa1f461c62a
Quyen Ly <quocquyenit@gmail.com>
2019-12-16 17:56:02 +0700

    RP-281 Corrected group name

--- src/app/components/snomed-navbar/snomed-navbar.component.ts
+++ src/app/components/snomed-navbar/snomed-navbar.component.ts
@@ -71,7 +71,7 @@ export class SnomedNavbarComponent implements OnInit {
 
         filteredProjects.sort(function(a, b) { return a['key'].localeCompare(b['key']); });
 
-        if (this.authenticationService.roles.includes('ROLE_sca-author')) {
+        if (this.authenticationService.roles.includes('ROLE_ihtsdo-sca-author')) {
             this.addProjectMAIN();
         }
 
